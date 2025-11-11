d7f12fef55401df7df42f6140523c2479ba79d1f
Rud <r.hildermann@outlook.de>
2025-11-11 12:22:39 +0100

    initial push

---
+++ 03-Python-Advanced/building_pyramid.py
@@ -0,0 +1,45 @@
+# # # Scenario # # #
+
+# Listen to this story: a boy and his father, a computer programmer, are playing with
+#  wooden blocks. They are building a pyramid.
+
+# Their pyramid is a bit weird, as it is actually a pyramid-shaped wall - it's flat.
+#  The pyramid is stacked according to one simple principle: each lower layer contains
+#  one block more than the layer above.
+
+# Your task is to write a program which reads the number of blocks the builders have,
+# and outputs the height of the pyramid that can be built using these blocks.
+
+# Note: the height is measured by the number of fully completed layers - if the builders
+# don't have a sufficient number of blocks and cannot complete the next layer, they finish
+# their work immediately.
+
+# _________________________________________________________________________________________
+
+# # # Test Data # # #
+
+# Sample input: 6
+# Expected output: The height of the pyramid: 3
+
+# Sample input: 20
+# Expected output: The height of the pyramid: 5
+
+# Sample input: 1000
+# Expected output: The height of the pyramid: 44
+
+# Sample input: 2
+# Expected output: The height of the pyramid: 1
+
+# _________________________________________________________________________________________
+
+# # # Logic # # #
+
+blocks = int(input("Enter the number of blocks: "))
+layer = 1
+height = 0
+
+while blocks >= layer:
+    blocks -= layer
+    layer += 1
+    height += 1
+print("The height of the pyramid:", height)
--- 03-Python-Advanced/collatz_hypothesis.py
+++ 03-Python-Advanced/collatz_hypothesis.py
@@ -10,14 +10,51 @@
 
 # The hypothesis says that regardless of the initial value of c0, it will always go to 1.
 
-# Of course, it's an extremely complex task to use a computer in order to prove the hypothesis for 
-# any natural number (it may even require artificial intelligence), but you can use Python to check
-# some individual numbers. Maybe you'll even find the one which would disprove the hypothesis.
+# Of course, it's an extremely complex task to use a computer in order to prove the hypothesis
+# for any natural number (it may even require artificial intelligence), but you can use Python
+# to check some individual numbers. Maybe you'll even find the one which would disprove the
+# hypothesis.
 
-# Write a program which reads one natural number and executes the above steps as long as c0 remains 
-# different from 1. We also want you to count the steps needed to achieve the goal. Your code should 
-# output all the intermediate values of c0, too.
+# Write a program which reads one natural number and executes the above steps as long as c0
+# remains different from 1. We also want you to count the steps needed to achieve the goal. 
+# Your code should output all the intermediate values of c0, too.
 
+# _________________________________________________________________________________________
+
+# # # Test Data # # #
+
+# Sample input: 15
+# Expected output:
+# 46
+# 23
+# 70
+# 35
+# 106
+# 53
+# 160
+# 80
+# 40
+# 20
+# 10
+# 5
+# 16
+# 8
+# 4
+# 2
+# 1
+# steps = 17
+
+# Sample input: 16
+# Expected output:
+# 8
+# 4
+# 2
+# 1
+# steps = 4
+
+# _________________________________________________________________________________________
+
+# # # Logic # # #
 
 c0 = int(input())
 steps = 0
